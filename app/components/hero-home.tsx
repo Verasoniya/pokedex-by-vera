4a2e742af30117b28deb3e3b57654b28dce17571
Vera Soniya <septiverasoniya99@gmail.com>
2026-06-03 17:10:17 +0700

    feat: create hero home component

---
+++ app/components/hero-home.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const HeroHome = () => {
+  return (
+    <button className="relative cursor-pointer ">
+      <img
+        style={{ imageRendering: 'auto' }}
+        src="/assets/svg/gap-circle-green-2.svg"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-[spin_3s_linear_infinite]"
+        alt=""
+      />
+
+      <img
+        style={{ imageRendering: 'auto' }}
+        src="/assets/svg/gap-circle-green.svg"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-[spin_2s_linear_infinite_reverse]"
+        alt=""
+      />
+
+      <img
+        src="/assets/svg/pokeball-line.svg"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+        alt=""
+      />
+
+      <img
+        src="/assets/image/poke.png"
+        className="absolute left-1/2 top-1/2 w-96 h-96 -translate-x-1/2 -translate-y-1/2 animate-poke-enter -hover:scale-[1.1]"
+        alt=""
+      />
+    </button>
+  );
+};
+
+export default HeroHome;
--- app/explore/explore.tsx
+++ app/explore/explore.tsx
@@ -1,4 +1,5 @@
 import { Droplets } from 'lucide-react';
+import HeroHome from '~/components/hero-home';
 import PokeCircleCard from '~/components/poke-circle-card';
 import SearchInput from '~/components/search';
 import TypeBadge from '~/components/type-badge';
@@ -29,31 +30,7 @@ export function Explore() {
           </div>
         </div>
       </div>
-      <div className="relative">
-        <img
-          src="/assets/svg/gap-circle-green-2.svg"
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-[spin_3s_linear_infinite]"
-          alt=""
-        />
-
-        <img
-          src="/assets/svg/gap-circle-green.svg"
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-[spin_2s_linear_infinite_reverse]"
-          alt=""
-        />
-
-        <img
-          src="/assets/svg/pokeball-line.svg"
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-          alt=""
-        />
-
-        <img
-          src="/assets/image/poke.png"
-          className="absolute left-1/2 top-1/2 w-96 h-96 -translate-x-1/2 -translate-y-1/2"
-          alt=""
-        />
-      </div>
+      <HeroHome />
     </main>
   );
 }
