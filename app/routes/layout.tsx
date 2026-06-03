7171f01bd1ef6bd0a5f72c6324c62699b17b12cb
Vera Soniya <septiverasoniya99@gmail.com>
2026-06-04 05:54:29 +0700

    fix: mobile responsive

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router';
 import Navbar from '~/components/navbar';
 import type { Route } from './+types/home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,16 +10,19 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+const queryClient = new QueryClient();
 function Layout() {
   return (
-    <main className="min-h-screen pb-20">
-      <div className="flex flex-col gap-4">
-        <Navbar />
-        <section className="flex-1 px-20">
-          <Outlet />
-        </section>
-      </div>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="min-h-screen pb-20">
+        <div className="flex flex-col gap-4">
+          <Navbar />
+          <section className="flex-1 px-4 md:px-10 lg:px-20">
+            <Outlet />
+          </section>
+        </div>
+      </main>
+    </QueryClientProvider>
   );
 }
 
