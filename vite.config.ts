5dfa0c986ca397cdda1705b2bff94c4e031354fd
Vera Soniya <septiverasoniya99@gmail.com>
2026-06-04 09:33:02 +0700

    fix: proxy api target

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
+import { defineConfig, loadEnv } from 'vite';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: `${import.meta.env.VITE_API_TARGET}`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v2'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [tailwindcss(), reactRouter()],
+    resolve: {
+      tsconfigPaths: true,
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_TARGET,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api/v2'),
+        },
       },
     },
-  },
+  };
 });
